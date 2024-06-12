e90379231ab1a637f141b6f657911bdb0b726c72
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-12 15:53:27 -0400

    feat: generate create ebook page

---
+++ src/app/_pages/create-ebook/create-ebook.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-ebook',
+  templateUrl: './create-ebook.component.html',
+  styles: [
+  ]
+})
+export class CreateEbookComponent {
+
+}
