31bfe5f3ec80a9d88401d964b2f0d2e63a25ab3f
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-12 15:53:46 -0400

    feat: generate ebook list page

---
+++ src/app/_pages/ebook-list/ebook-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ebook-list',
+  templateUrl: './ebook-list.component.html',
+  styles: [
+  ]
+})
+export class EbookListComponent {
+
+}
