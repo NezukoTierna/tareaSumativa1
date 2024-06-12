9d42bc4655f94778407ce3beeb8eb9511461beb0
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-12 15:52:31 -0400

    feat: add declarations and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { TextInputComponent } from './_components/text-input/text-input.component';
+import { SelectInputComponent } from './_components/select-input/select-input.component';
+import { EbookListComponent } from './_pages/ebook-list/ebook-list.component';
+import { CreateEbookComponent } from './_pages/create-ebook/create-ebook.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextInputComponent,
+    SelectInputComponent,
+    EbookListComponent,
+    CreateEbookComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
